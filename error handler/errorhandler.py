39bff818eb761175a6482422bc585115e8e98c5c
TOG6-6 <67944107+TOG6-6@users.noreply.github.com>
2020-10-06 18:23:19 +0530

    Add files via upload

---
+++ error handler/errorhandler.py
@@ -0,0 +1,49 @@
+#your usual discord imports
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        await ctx.send("**`Command doesn't exist!`**")
+        return
+
+    if isinstance(error, commands.CommandOnCooldown):
+        coolerrr = "**`Mate `" + (ctx.author.mention) + "` You are on cooldown for {:.2f}s. Please Be Patient!`**".format(error.retry_after)
+        await ctx.send(coolerrr)
+        return
+
+    if isinstance(error, commands.BotMissingPermissions):
+            missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_perms]
+            if len(missing) > 2:
+                fmt = '{}, and {}'.format("**, **".join(missing[:-1]), missing[-1])
+            else:
+                fmt = ' and '.join(missing)
+            _message = '**I need the `{}` permission(s) to run this command.**'.format(fmt)
+            await ctx.send(_message)
+            return
+
+    if isinstance(error, commands.CheckFailure):
+            await ctx.send("**You do not have permission to use this command.**" + ctx.author.mention)
+            return
+    
+    if isinstance(error, commands.MissingPermissions):
+        missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_perms]
+        if len(missing) > 2:
+            fmt = '{}, and {}'.format("**, **".join(missing[:-1]), missing[-1])
+            return
+        else:
+            fmt = ' and '.join(missing)
+        _message = 'You need the **{}** permission(s) to use this command.'.format(fmt)
+        await ctx.send(_message)
+        return
+
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("**`You Need To Give The Required Statement After The Command Bruh, `" + (ctx.author.mention) + "**")
+        return
+    
+    if isinstance(error, commands.BadArgument):
+        await ctx.send("**`SOME ERRNO`**")
+        return
+
+    if isinstance(error, AttributeError):
+        return
+
+    raise error
