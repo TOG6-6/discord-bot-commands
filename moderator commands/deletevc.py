5eb059376eda277e864024f62a2831a10f58c29b
TOG6-6 <67944107+TOG6-6@users.noreply.github.com>
2020-11-06 14:40:50 +0530

    Update deletevc.py

--- moderator commands/deletevc.py
+++ moderator commands/deletevc.py
@@ -1,6 +1,5 @@
 @client.command()
 @commands.has_guild_permissions(manage_channels=True)
 async def deletevc(ctx, *, mane: discord.VoiceChannel):
-    if statcheck() == "1":
-        await mane.delete()
-        await ctx.send(f"**Successfully Deleted Voice Channel - `{mane}`**")
+    await mane.delete()
+    await ctx.send(f"**Successfully Deleted Voice Channel - `{mane}`**")
