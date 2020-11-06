34142510951bf125e782996c0439aab1ea9d0437
TOG6-6 <67944107+TOG6-6@users.noreply.github.com>
2020-11-06 14:35:41 +0530

    Create snipe.py

---
+++ entertainment commands/snipe.py
@@ -0,0 +1,18 @@
+deletedcontent = []
+deleteduser = []
+@client.event
+async def on_message_delete(message):
+    if statcheck() == "1":
+        deletedcontent.append(message.content)
+        deleteduser.append(message.author)
+        
+@client.command()
+async def snipe(ctx):
+    if statcheck() == "1":
+        k = deletedcontent[-1]
+        v = deleteduser[-1]
+        try:
+            await ctx.send("**`" + k + "`**")
+            await ctx.send("- " + str(v))
+        except KeyError:
+            await ctx.send("Eh, Nothing To Snipe Here")
