01a27cb5f6f6e1a75ca36c7380a4c67e940b29fa
TOG6-6 <67944107+TOG6-6@users.noreply.github.com>
2020-11-06 14:40:31 +0530

    Update createvc.py

--- moderator commands/createvc.py
+++ moderator commands/createvc.py
@@ -1,7 +1,6 @@
 @client.command()
 @commands.has_guild_permissions(manage_channels=True)
 async def createvc(ctx, *, mane):
-    if statcheck() == "1":
-        guild = ctx.guild
-        await guild.create_voice_channel(name=mane)
-        await ctx.send(f"**Successfully Created Voice Channel - `{mane}`**")
+    guild = ctx.guild
+    await guild.create_voice_channel(name=mane)
+    await ctx.send(f"**Successfully Created Voice Channel - `{mane}`**")
