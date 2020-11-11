7e8a087c7711d537cebbf1bac8fc1659e95cdd1d
TOG6-6 <67944107+TOG6-6@users.noreply.github.com>
2020-11-11 12:53:49 +0530

    Update mainbotcode.py

--- mainbotcode.py
+++ mainbotcode.py
@@ -24,24 +24,21 @@ ownerdude = () #add your discord id here - not token, discord id
 client = commands.Bot(command_prefix='$')#thats your command prefix
 #whatever code you want to add, do it after this line
 
+now = datetime.now()
+
 @client.event
 async def on_ready():
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     print(Fore.LIGHTCYAN_EX +"""
-████████╗████████╗  ░░░░░░  ██████╗░░█████╗░████████╗
-╚══██╔══╝╚══██╔══╝  ░░░░░░  ██╔══██╗██╔══██╗╚══██╔══╝
-░░░██║░░░░░░██║░░░  █████╗  ██████╦╝██║░░██║░░░██║░░░
-░░░██║░░░░░░██║░░░  ╚════╝  ██╔══██╗██║░░██║░░░██║░░░
-░░░██║░░░░░░██║░░░  ░░░░░░  ██████╦╝╚█████╔╝░░░██║░░░
-░░░╚═╝░░░░░░╚═╝░░░  ░░░░░░  ╚═════╝░░╚════╝░░░░╚═╝░░░""")
+Bot is ready, use some ascii or something to make this part look cool""")
     print(Fore.MAGENTA +"======================================================")
     print(Fore.RED +"Logged in as:", client.user.name, "| ID:", client.user.id)
     print(Fore.MAGENTA +"======================================================")
     kioas = str(len(client.guilds))
-    print(Fore.CYAN + "Present In The Tog6 Server!")
-    await client.change_presence(status=discord.Status.online, activity=discord.Game("I am TOG Mate, DM me, not anyone else XD!"))
-    print(Fore.BLUE + "I am TOG Mate, DM me, not anyone else XD!")
+    print(Fore.CYAN + f"Present In {kioas} Servers!")
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("Status you want the bot to have!"))
+    print(Fore.BLUE + "Status you want the bot to have!")
     print(Fore.LIGHTYELLOW_EX + "TT - BOT was started at: " + current_time)
     print(" ")
     print(Fore.GREEN +"Built by TOG6")
